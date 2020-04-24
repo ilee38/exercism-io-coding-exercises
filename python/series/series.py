0f07bfade5e265b61aef20d2f034dc6868d237bc
Iram Lee <iram.lee@gmail.com>
2020-04-24 07:32:08 -0500

    implement solution to series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,17 @@
+
 def slices(series, length):
-    pass
+    """ param series = string sequence of numbers
+        param length = length of slices to return
+    """
+    if length > len(series) or length <= 0:
+        raise ValueError("Invalid parameter: length")
+
+    sequences = []
+    i = 0
+    while True:
+        if i + length <= len(series):
+            sequences.append(series[i:i+length])
+            i += 1
+        else:
+            break
+    return sequences
